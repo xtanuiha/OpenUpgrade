8fd7d9b53fcc1434675f1d43d56719c3f5263863
feihu.zhang <feihu.zhang@yumtown.com.cn>
2024-06-21 08:03:38 +0000

    [IMP] Migration quantity data in stock.move and stock.move.line

--- openupgrade_scripts/scripts/stock/17.0.1.1/post-migration.py
+++ openupgrade_scripts/scripts/stock/17.0.1.1/post-migration.py
@@ -5,6 +5,32 @@
 from openupgradelib import openupgrade
 
 
+def _update_stock_move_quantity(env):
+    openupgrade.logged_query(
+        env,
+        "UPDATE stock_move SET quantity = quantity_done WHERE quantity_done != 0;"
+    )
+
+
+def _update_stock_move_scrap(env):
+    sql = 'SELECT id,move_id FROM stock_scrap WHERE move_id IS NOT NULL;'
+    env.cr.execute(sql)
+    scrap_dict = env.cr.dictfetchall()
+    scrap_dict = dict([(item['id'], item['move_id']) for item in scrap_dict])
+    for scrap in env['stock.scrap'].search([('id', 'in', list(scrap_dict.keys()))]):
+        scrap.write({'move_ids': [(4, scrap_dict[scrap.id])]})
+
+
+def _update_stock_move_line_quantity(env):
+    openupgrade.logged_query(
+        env,
+        "UPDATE stock_move_line SET quantity = qty_done WHERE qty_done != 0;"
+    )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(env, "stock", "17.0.1.1/noupdate_changes.xml")
+    _update_stock_move_quantity(env)
+    _update_stock_move_scrap(env)
+    _update_stock_move_line_quantity(env)
